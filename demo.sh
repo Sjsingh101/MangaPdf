f89070aac0e0f3b09905bf62f66871d7e4a722da
Simranjeet Singh <sjsingh9090@gmail.com>
2021-03-14 13:01:51 +0530

    Script Restructured

---
+++ demo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 mangapdf.py -u https://kodansha.us/series/a-silent-voice/
--- mangapdf.py
+++ mangapdf.py
@@ -5,61 +5,44 @@ import os
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 
-
-parser = argparse.ArgumentParser()
-parser_group = parser.add_argument_group('required named arguments')
-parser_group.add_argument("-u", "--URL", help = "Add Manga URL",required=True)
- 
-args = parser.parse_args()
- 
-if args.URL:
-    response = req.get(args.URL) 
-
-
-# %%
-iurl = "https://kodansha.us/series/a-silent-voice/"
-response = req.get(iurl)
-
-
-# %%
-soup = BeautifulSoup(response.text)
-mid = soup.find("manga-reader")["chapter-id"]
-
-
-# %%
 jurl = "https://kodansha.us/wp-content/themes/kodansha-usa/chapter-json.php"
 Image_dir = "images/"
 Output_dir = "output/"
 
+def getimagejson(url):
+    response = req.get(url)
+    soup = BeautifulSoup(response.text,features="lxml")
+    mid = soup.find("manga-reader")["chapter-id"]
+    payload={'id': mid}
+    response = req.post(jurl,data=payload)  
+    data = response.json()
+    return data
 
-# %%
-payload={'id': mid}
-response = req.post(jurl,data=payload)
-
-
-# %%
-data = response.json()
-name = data["chapter_volume"]
-url_list = data['chapter_pages']
-
-
-# %%
-chapter_img_path = Image_dir+name+"/"
-try:
-    os.mkdir(chapter_img_path)
-except:
-    print("Folder Present")
-
-
-# %%
 def download(purl, file_loc):
     img_res = req.get(purl)
     if img_res.status_code == 200:
         with open(file_loc, 'wb') as ifile:
             ifile.write(img_res.content)
 
+def download_manga_images(url):
+    data = getimagejson(url) 
+    name = data["chapter_volume"]
+    url_list = data['chapter_pages']
 
-# %%
+    chapter_img_path = Image_dir+name+"/"
+    
+    try:
+        os.mkdir(chapter_img_path)
+        for ind,obj in list(enumerate(url_list)):
+            purl = obj['url']
+            img_path = chapter_img_path + str(ind) + ".jpg"
+            download_thread = threading.Thread(target=download, args=(purl,img_path))
+            download_thread.start()
+    except:
+        print("Manga Present")
+
+    return name
+
 def export_pdf(name):
     pdf = FPDF('P','mm','A4')
     x,y,w,h = 0,0,210,297
@@ -74,13 +57,16 @@ def export_pdf(name):
     pdf.output(Output_dir+name+".pdf","F")
 
 
-# %%
-for ind,obj in list(enumerate(url_list)):
-    purl = obj['url']
-    img_path = chapter_img_path + str(ind) + ".jpg"
-    download_thread = threading.Thread(target=download, args=(purl,img_path))
-    download_thread.start()
 
-export_pdf(name)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser_group = parser.add_argument_group('required named arguments')
+    parser_group.add_argument("-u", "--URL", help = "Add Manga URL",required=True)
+    
+    args = parser.parse_args()
+    
+    if args.URL:
+        name = download_manga_images(args.URL)
+        export_pdf(name)
 
 
